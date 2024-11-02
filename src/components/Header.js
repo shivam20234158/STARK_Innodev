6869dfa93c6f6aedb75e2b3efb74f47ecace7b55
shivam20234158 <shivampanwar491@gmail.com>
2024-11-02 12:41:07 +0530

    some more homepage features

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoImage from "../images/Designer_logo.png";
 
 function Header() {
   return (
     <header>
-      <h1>H.E.L.M. (Hostel Environment & Living Management)</h1>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/feedback">Submit Feedback</Link>
-        <Link to="/admin">Admin Dashboard</Link>
-      </nav>
+      <div className="container">
+        <div className="home_link">
+          <Link to="/">
+            <img src={logoImage} alt="Logo" className="logo" />
+          </Link>
+          <h1>
+            <Link to="/">H.E.L.M. (Hostel Environment & Living Management)</Link>
+          </h1>
+        </div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/feedback">Submit Feedback</Link>
+          <Link to="/admin">Admin Dashboard</Link>
+        </nav>
+      </div>
     </header>
   );
 }
